471b84cf7b8e734c808cbc25d28dbce152af7b20
Aditya Vardan G <adityavardan31993@gmail.com>
2026-09-11 15:59:05 +0530

    fix(auth): register forgot password route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,23 +2,28 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login';
 import { DashboardComponent } from './features/dashboard/dashboard';
 import { SignupComponent } from './features/auth/signup/signup';
+import { ForgotPasswordComponent } from './features/auth/forgot-password/forgot-password';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'login',
-        pathMatch: 'full',
-    },
-    {
-        path: 'login',
-        component: LoginComponent,
-    },
-    {
-        path: 'dashboard',
-        component: DashboardComponent,
-    },
-    {
-        path: 'signup',
-        component: SignupComponent,
-    }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+  },
 ];
