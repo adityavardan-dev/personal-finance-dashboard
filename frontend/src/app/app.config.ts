5a64be05c06c9a44193527eb6cf71fd511695c68
Aditya Vardan G <adityavardan31993@gmail.com>
2026-09-11 16:49:01 +0530

    feat(frontend): register JWT HTTP interceptor

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,15 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
-
-import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { routes } from './app.routes';
+import { authInterceptor } from './core/auth/auth.interceptor';
+
 export const appConfig: ApplicationConfig = {
-  // The providers array is used to configure the application with various services and features.
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(),
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
 };
