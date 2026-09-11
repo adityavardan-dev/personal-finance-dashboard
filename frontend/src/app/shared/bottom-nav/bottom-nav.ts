bff7acb2e41bf883dcc40a6c44f9d5ec1b6a9545
Aditya Vardan G <adityavardan31993@gmail.com>
2026-09-11 15:11:35 +0530

    refactor(ui): wire accessible mobile navigation

--- frontend/src/app/shared/bottom-nav/bottom-nav.ts
+++ frontend/src/app/shared/bottom-nav/bottom-nav.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-bottom-nav',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './bottom-nav.html',
   styleUrl: './bottom-nav.css',
 })
