22b3ee29ca3c88d0e2ea0aa31150c1e97de914c7
Aditya Vardan G <adityavardan31993@gmail.com>
2026-09-11 15:58:39 +0530

    feat(auth): add forgot password page

---
+++ frontend/src/app/features/auth/forgot-password/forgot-password.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-forgot-password',
+  imports: [RouterLink],
+  templateUrl: './forgot-password.html',
+  styleUrl: './forgot-password.css',
+})
+export class ForgotPasswordComponent {
+  protected submitted = false;
+
+  protected submit(): void {
+    this.submitted = true;
+  }
+}
