a8b994e1bd598ea81d2678cc592c7ceff2066feb
Aditya Vardan G <adityavardan31993@gmail.com>
2026-09-21 17:11:46 +0530

    chore: remove unused user service import

--- backend/src/auth/users.service.ts
+++ backend/src/auth/users.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
-import { randomUUID } from 'crypto';
 
 export interface User {
   id: number;
