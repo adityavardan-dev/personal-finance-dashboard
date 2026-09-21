524755f6539fe38b595e49271b4885dd7c6fd2da
Aditya Vardan G <adityavardan31993@gmail.com>
2026-09-21 17:12:03 +0530

    feat: register persisted user service

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
@@ -14,6 +15,6 @@ import { JwtStrategy } from './jwt.strategy';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, UsersService],
 })
 export class AuthModule {}
